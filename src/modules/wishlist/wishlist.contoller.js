c19e7e29c7b9f05ee65ad10f17e4369879fe538a
mahmoud <mhmwdbhjt307@gmail.com>
2024-10-10 00:00:08 +0300

    final wishlist

--- src/modules/wishlist/wishlist.contoller.js
+++ src/modules/wishlist/wishlist.contoller.js
@@ -1,7 +1,7 @@
 import { User } from "../../../db/index.js";
 import { messages } from "../../utils/constant/messages.js";
 
-
+// add and updated wishlist
 export const addToWishList = async (req, res, next) => {
   // get data from req
   const { productId } = req.params;
@@ -12,8 +12,59 @@ export const addToWishList = async (req, res, next) => {
     {new : true}
   );
   return res.status(200).json({
-      message: messages.,
+      message: messages.wishList.updateSuccessfully,
       success: true,
       data: userUpdated.wishList,
     });
+};
+
+// Get all and specifice  wishlist items for a user
+export const getAllWishList = async (req, res, next) => {
+  try {
+    const userId = req.authUser._id; 
+    const user = await User.findById(userId).populate("wishList"); 
+
+    if (!user) {
+      return res.status(404).json({
+        message: messages.user.notFound,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: messages.wishList.getAllSuccessfully,
+      success: true,
+      data: user.wishList,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete item from wishlist
+export const deleteFromWishList = async (req, res, next) => {
+  try {
+    const { productId } = req.params; // Get the product ID from request parameters
+    const userId = req.authUser._id; // Get the user ID from the authenticated user
+
+    // Remove the product from the wishlist
+    const userUpdated = await User.findByIdAndUpdate(userId, {
+      $pull: { wishList: productId } // Use $pull to remove the product from the wishlist
+    }, { new: true });
+
+    if (!userUpdated) {
+      return res.status(404).json({
+        message: messages.user.notFound,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: messages.wishList.deleteSuccessfully,
+      success: true,
+      data: userUpdated.wishList,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/modules/wishlist/wishlist.router.js
+++ src/modules/wishlist/wishlist.router.js
@@ -3,14 +3,28 @@ import { isAuthenticated } from "../../middleware/authentication.js";
 import { isAuthorized } from "../../middleware/autheraization.js";
 import { roles } from "../../utils/constant/enums.js";
 import { asyncHandler } from "../../utils/appError.js";
-import { addToWishList } from "./wishlist.contoller.js";
+import { addToWishList, deleteFromWishList, getAllWishList } from "./wishlist.contoller.js";
 
 const wishlistRouter = Router()
 
-// add to wishlist
+// add and update to wishlist
 wishlistRouter.post('/:productId',
   isAuthenticated(),
   isAuthorized([roles.USER, roles.ADMIN]),
   asyncHandler(addToWishList)
 )
+
+// get all wishlist
+wishlistRouter.get('/',
+  isAuthenticated(),
+  isAuthorized([roles.USER, roles.ADMIN]),
+  asyncHandler(getAllWishList)
+)
+
+// Delete an item from the wishlist
+wishlistRouter.delete('/:productId',
+  isAuthenticated(), 
+  isAuthorized([roles.USER, roles.ADMIN]), 
+  asyncHandler(deleteFromWishList) 
+);
 export default wishlistRouter 
